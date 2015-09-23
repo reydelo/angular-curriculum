a06c8f25e108a8ccda29dd8dc2371bbc36607203
reydelo <reyna.deloge@gmail.com>
2015-09-23 13:05:50 -0600

    query url params in controller

--- Unit-2/js/app.js
+++ Unit-2/js/app.js
@@ -2,17 +2,21 @@ var app = angular.module("myApp", ['ngRoute']);
 
 app.config(function($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'partials/home.html',
-        controller: 'HomeController'
+      // .when('/', {
+      //   templateUrl: 'partials/home.html',
+      //   controller: 'HomeController'
+      // })
+      // .when('/dogs', {
+      //   templateUrl: 'partials/dogs.html',
+      //   controller: 'DogsController'
+      // })
+      // .when('/projects', {
+      //   templateUrl: 'partials/projects.html',
+      //   controller: 'ProjectsController'
+      // })
+      .when('/calculator/:operator/', {
+        templateUrl: 'partials/calculator.html',
+        controller: 'CalculatorController'
       })
-      .when('/dogs', {
-        templateUrl: 'partials/dogs.html',
-        controller: 'DogsController'
-      })
-      .when('/projects', {
-        templateUrl: 'partials/projects.html',
-        controller: 'ProjectsController'
-      })
-      .otherwise({redirectTo: '/'});
+      // .otherwise({redirectTo: '/'});
 });
--- Unit-2/js/controllers.js
+++ Unit-2/js/controllers.js
@@ -15,3 +15,21 @@ app.controller("MainController", function($scope, $location) {
     return url === $location.path();
   }
 });
+
+app.controller("CalculatorController", function($scope, $routeParams) {
+  console.log($routeParams);
+  var operator = $routeParams.operator;
+  var x = +$routeParams.x;
+  var y = +$routeParams.y;
+  console.log(operator);
+  if (operator == "add") {
+    console.log("hey!")
+    $scope.answer = x + y;
+  }
+  if (operator == "division") {
+    $scope.answer = x / y;
+  }
+  if (!x) {
+    $scope.answer = "You fucked up!"
+  }
+})
